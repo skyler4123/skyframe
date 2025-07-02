55e70a9f2a476f96caf766de94569e682bc6e5fc
skyler <pkphung2001@gmail.com>
2025-07-02 21:55:10 +0700

    update

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
-export default function ProductsIdPage(props: Props) {
-
-  useEffect(() => {
-    console.log("Product ID:", props);
-    // Here you can fetch product details using the id
-  }, []);
+export default function ProductsIdPage({ params }: Props) {
+  const { id } = use(params);
+  // useEffect(() => {
+  //   // Here you can fetch product details using the id
+  //   console.log(params.id)
+  // }, []);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Products
+      Products Page {id}
     </div>
   )
 }
